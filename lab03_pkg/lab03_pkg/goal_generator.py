ccfba5be365ef990ef80f556b33c17c88afbffbe
BoulosMansour <72452999+BoulosMansour@users.noreply.github.com>
2023-11-30 18:24:38 +0100

    Add files via upload

---
+++ lab03_pkg/lab03_pkg/goal_generator.py
@@ -0,0 +1,188 @@
+# This template is derived from ROS 2 action client example. You can find the complete example here:
+# https://github.com/ros2/examples/blob/master/rclpy/actions/minimal_action_client/examples_rclpy_minimal_action_client/client_asyncio.py
+
+
+import rclpy
+import rclpy.logging
+from rclpy.node import Node
+from rclpy.action import ActionClient
+
+from math import degrees
+
+from action_msgs.msg import GoalStatus
+from lab03_interfaces.srv import ComputeTrajectory
+from lab03_interfaces.action import MoveForward, RotateAbsolute
+
+import asyncio
+
+
+class GoalGeneratorActionClient(Node):
+    def __init__(self):
+        super().__init__("goal_generator_action_client")
+
+        # Create a Service client instance
+        self.compute_traj_cli = self.create_client(ComputeTrajectory, "compute_trajectory")
+        # Create an ActionClient instance
+        self.rotate_absolute_client = ActionClient(self, RotateAbsolute, "rotate_absolute")
+        self.move_forward_client = ActionClient(self, MoveForward, "move_forward")
+
+        # Example to set the log level from a parameter
+        log_level = self.declare_parameter("log_level", "info")
+        if log_level.get_parameter_value().string_value == "debug":
+            self.get_logger().set_level(rclpy.logging.LoggingSeverity.DEBUG)
+        else:
+            self.get_logger().set_level(rclpy.logging.LoggingSeverity.INFO)
+
+    def rotate_absolute_get_feedback(self, feedback_msg):
+        '''
+        Callback function to get action feedback callback
+        '''
+        feedback = feedback_msg.feedback
+        # the throttle duration is used to limit the rate at which the message is printed
+        self.get_logger().info(f"Remaining: {feedback.remaining_angle} degrees", throttle_duration_sec=1.0)
+
+    def move_forward_get_feedback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        self.get_logger().info(f" speed: {feedback.linear_velocity} m/s", throttle_duration_sec=1.0)
+
+    # Async call of send goal function (call action server)
+    async def send_goal(self, client: ActionClient, goal, feedback_callback):
+        '''
+        Async function to send goal to an action server
+
+        Arguments:
+            client {ActionClient} -- Action client instance
+            goal {object} -- Goal object
+            feedback_callback {function} -- Callback function to get feedback
+
+        Returns:
+            result {object or None} -- Result object
+            status {int or None} -- Status code
+        '''
+        self.get_logger().info("Waiting for action server...")
+        client.wait_for_server()
+
+        self.get_logger().info("Sending goal request...")
+
+        goal_handle = await client.send_goal_async(goal, feedback_callback=feedback_callback)
+
+        if not goal_handle.accepted:
+            self.get_logger().warn("Goal rejected")
+            return None, None
+        
+        action_result = await goal_handle.get_result_async()
+
+        result = action_result.result
+        status = action_result.status
+
+        if status != GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().warn("Goal failed with status: {0}".format(status))
+
+        return result, status
+
+
+async def spinning(node):
+    '''
+    Async function to spin the node. 
+    This function is used to spin the node inside the run function
+    '''
+    while rclpy.ok():
+        rclpy.spin_once(node, timeout_sec=0.01)
+        await asyncio.sleep(0.001)
+
+
+async def run(args, loop):
+    '''
+    The function where you can write your code
+    '''
+    rclpy.init(args=args)
+
+    # Instantiate the node and spin it
+    goal_generator = GoalGeneratorActionClient()
+    spin_task = loop.create_task(spinning(goal_generator))
+
+    ################################################################################################
+    # YOUR CODE SECTION STARTS HERE
+    ################################################################################################
+    goal = [6.5, 2.5, 90]
+
+    goal_generator.get_logger().info("Sending goal: " + str(goal))
+
+    # Example of calling a SERVICE using async/await
+    goal_generator.compute_traj_cli.wait_for_service()
+    request = ComputeTrajectory.Request()
+    request.x = goal[0]
+    request.y = goal[1]
+    request.angle = goal[2]
+    future = goal_generator.compute_traj_cli.call_async(request)
+    response = await future
+    goal_generator.get_logger().info(f'step: {response.step} response_angle: {response.angle} degrees')
+
+    while response.step<5:
+        goal_direction = RotateAbsolute.Goal()
+        goal_direction.angle = response.angle
+        rotate_result, rotate_status = await loop.create_task(
+            goal_generator.send_goal(
+                goal_generator.rotate_absolute_client,
+                goal_direction,
+                goal_generator.rotate_absolute_get_feedback,
+            )
+        )
+
+        goal_position = MoveForward.Goal()
+        goal_position.x = goal[0]
+        goal_position.y = goal[1]
+        goal_position.step = response.step
+        move_result, move_status = await loop.create_task(
+            goal_generator.send_goal(
+                goal_generator.move_forward_client,
+                goal_position,
+                goal_generator.move_forward_get_feedback,    
+            )
+        )
+
+        goal_generator.compute_traj_cli.wait_for_service()
+        request = ComputeTrajectory.Request()
+        request.x = goal[0]
+        request.y = goal[1]
+        request.angle = goal[2]
+        future = goal_generator.compute_traj_cli.call_async(request)
+        response = await future
+        goal_generator.get_logger().info(f'step: {response.step} response_angle: {response.angle} degrees')
+            
+    # Example of calling the rotate absolute ACTION SERVER using async/await
+    goal_angle = RotateAbsolute.Goal()
+    goal_angle.angle = response.angle
+    result, status = await loop.create_task(
+            goal_generator.send_goal(
+                goal_generator.rotate_absolute_client,
+                goal_angle,
+                goal_generator.rotate_absolute_get_feedback,
+            )
+    )
+        
+    goal_generator.get_logger().info("All goals completed! Shutting down...")
+    spin_task.cancel()
+
+    ################################################################################################
+    # YOUR CODE SECTION END HERE
+    ################################################################################################
+
+    # Wait for the spinning task to finish and destroy the node
+    try:
+        await spin_task
+    except asyncio.exceptions.CancelledError:
+        pass
+    finally:
+        goal_generator.destroy_node()
+        rclpy.try_shutdown()
+
+
+def main(args=None):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(args, loop))
+    loop.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ lab03_pkg/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'lab03_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros',
+    maintainer_email='ros@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'compute_trajectory = lab03_pkg.compute_trajectory:main',
+                'move_forward = lab03_pkg.move_forward:main',
+                'rotate_absolute = lab03_pkg.rotate_absolute:main',
+                'goal_generator = lab03_pkg.goal_generator:main'
+        ],
+    },
+)
